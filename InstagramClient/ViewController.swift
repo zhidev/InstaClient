b460dad5908a7d9bf553e59eddcd6b315a7a2ffa
YOUR NAME <anuragNandasf@gmail.com>
2016-01-21 19:16:55 -0800

    Initial Commit

---
+++ InstagramClient/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  InstagramClient
+//
+//  Created by Nate nanda on 1/21/16.
+//  Copyright © 2016 AnuragNanda. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
