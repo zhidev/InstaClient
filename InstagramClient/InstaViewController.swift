79029dfbdee8a81666abbc34a328c70b0ba4c86b
zhidev <Doug@douglass-macbook-air.local>
2016-01-21 19:53:14 -0800

    Milestone 2

--- InstagramClient/InstaViewController.swift
+++ InstagramClient/InstaViewController.swift
@@ -11,9 +11,35 @@ import AFNetworking
 
 class InstaViewController: UIViewController {
 
+    var results: [NSDictionary]?
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        let clientId = "e05c462ebd86446ea48a5af73769b602"
+        let url = NSURL(string:"https://api.instagram.com/v1/media/popular?client_id=\(clientId)")
+        let request = NSURLRequest(URL: url!)
+        let session = NSURLSession(
+            configuration: NSURLSessionConfiguration.defaultSessionConfiguration(),
+            delegate:nil,
+            delegateQueue:NSOperationQueue.mainQueue()
+        )
+        
+        let task : NSURLSessionDataTask = session.dataTaskWithRequest(request,
+            completionHandler: { (dataOrNil, response, error) in
+                if let data = dataOrNil {
+                    if let responseDictionary = try! NSJSONSerialization.JSONObjectWithData(
+                        data, options:[]) as? NSDictionary {
+                            NSLog("response: \(responseDictionary)")
+                            
+                            self.results = responseDictionary["data"] as! [NSDictionary]
+                            
+                    }
+                }
+        });
+        task.resume()
+        
     }
 
     override func didReceiveMemoryWarning() {
